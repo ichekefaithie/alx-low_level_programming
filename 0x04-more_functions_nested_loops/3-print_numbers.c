97de662a65f507e5542c8441f53d990fb4e7f827
ichekefaithie <faithgifted17@gmail.com>
2022-10-20 06:45:36 -0700

    C

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,15 +2,15 @@
 
 /**
  * print_numbers - prints number
+ * Return: Always 0
  */
 
 void print_numbers(void)
 {
-	int i;
+	int a = 0;
 
-	for (i = 48; i < 58; i++)
-	{
-		_putchar(i);
-	}
+	while (a < 10)
+		_putchar(a++ + '0');
+
 	_putchar('\n');
 }
